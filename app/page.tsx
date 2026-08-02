140b44ab0166d4bff9245b03e64c754cd35205f8
LoaizaBetancur <andresfelipe.loaizabetancur@adelaide.edu.au>
2026-08-02 18:18:34 +0930

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -131,7 +131,7 @@ export default function HomePage() {
           >
             <iframe
               style={{ position: "absolute", inset: 0, width: "100%", height: "100%", border: 0 }}
-              src="https://www.youtube.com/watch?v=D0Q9vypSYeE"
+              src="https://www.youtube.com/watch?v=g1MZi2mzK1U"
               title="CAPIS Project video"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
