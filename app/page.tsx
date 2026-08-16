c2ef1b4e766a097b4d2d76ca8c5bd136295d1ddc
LoaizaBetancur <andresfelipe.loaizabetancur@adelaide.edu.au>
2026-08-16 12:19:03 +0930

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 import ParticipantCard from "@/components/ParticipantCard";
 import StageCard from "@/components/StageCard";
 import RelatedProjects from "@/components/RelatedProjects";
-import RelatedProjects from "@/components/RelatedProjects";
-import RelatedProjects from "@/components/RelatedProjects";
+
 
 export const metadata: Metadata = {
   title: "Home",
