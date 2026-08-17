b990e284861e01ef8f4d7e2eabc33fc6237f8b8e
LoaizaBetancur <andresfelipe.loaizabetancur@adelaide.edu.au>
2026-08-17 21:57:41 +0930

    Update page.tsx

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -229,7 +229,7 @@ export default function TeamPage() {
             </p>
           </div>
           <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <div style={{ position: "relative", width: "280px", height: "220px" }}>
+            <div style={{ position: "relative", width: "500px", height: "400px" }}>
               <Image
                 src="/images/meet.png"
                 alt="Graphic representing the CAPIS project"
